ec9f6efb7c3126c12aa52cfcbda556226a958be1
Dmitry <nor808@yandex.ru>
2021-11-10 00:12:13 +0300

    2

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from typing import Optional
+from typing import Optional, List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author
 
 app = FastAPI()
 
 
-@app.get('/')
-def home():
-    return {"key": "hello"}
+# @app.get('/')
+# def home():
+#     return {"key": "hello"}
+#
+#
+# @app.get('/{pk}')
+# def get_item(pk: int, q: str = None):
+#     return {"key": pk, "q": q}
+#
+#
+# @app.get('/user/{pk}/items/{item}/')
+# def get_user_item(pk: int, item: str):
+#     return {"user": pk, "item": item}
 
 
-@app.get('/{pk}')
-def get_item(pk: int, q: str = None):
-    return {"key": pk, "q": q}
+@app.post('/book')
+def create_book(item: Book, author: Author, quantity: int = Body(...)):
+    return {"item": item, "author": author, "quantity":quantity}
+
+
+@app.post('/author')
+def create_author(author: Author = Body(..., embed=True)):
+    return {"author": author}
+
+@app.get('/book')
+def get_book(q: List[str] = Query(['test', 'test2'], description='book', deprecated=True)):
+    return q
+
+
+@app.get('/book/{pk}')
+def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le=500)):
+    return {'pk': pk, "pages": pages}
---
+++ schemas.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, validator, Field
+from datetime import date
+from typing import List
+
+
+class Genre(BaseModel):
+    name: str
+
+
+class Author(BaseModel):
+    first_name: str = Field(..., max_length=25)
+    last_name: str
+    age: int = Field(..., gt=15, lt=90, description='azaz?')
+
+    # @validator('age')
+    # def check_age(cls, v):
+    #     if v < 15 > 90:
+    #         raise ValueError('azaz')
+    #     return v
+
+
+class Book(BaseModel):
+    title: str
+    writer: str
+    duration: str
+    date: date
+    summary: str
+    genres: List[Genre]
+    pages: int
